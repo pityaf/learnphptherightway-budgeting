36f3e8858ef505bc46d102ae3593f4a0009540ca
Paolo Cantoreggi <paolo@DESKTOP-PAOLO>
2023-09-11 23:03:33 +0200

    1.32 finished

--- public/index.php
+++ public/index.php
@@ -9,3 +9,19 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+require APP_PATH . 'app.php';
+require APP_PATH . 'helper.php';
+
+$files = getTransactionFiles(FILES_PATH);
+
+$transactions = [];
+foreach($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file, 'parseTransaction'));
+}
+
+$totals = calculatoTotal($transactions);
+
+print_r($transactions);
+
+
+require VIEWS_PATH . 'transaction.php';
